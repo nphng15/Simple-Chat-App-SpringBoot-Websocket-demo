db85946bc8056e107eaecba8b8af28103985d587
Tran Nam Phong <phong98753@gmail.com>
2025-03-28 10:33:17 +0700

    updated

--- src/main/java/com/Thang/ChatApp/config/WebsocketEventListener.java
+++ src/main/java/com/Thang/ChatApp/config/WebsocketEventListener.java
@@ -24,7 +24,7 @@ public class WebsocketEventListener {
         StompHeaderAccessor headerAccessor = StompHeaderAccessor.wrap(event.getMessage());
         String username = (String) headerAccessor.getSessionAttributes().get("username");
 
-        if(username != null){
+        if (username != null) {
             log.info("User disconnected: {}", username);
             var chatMessage = ChatMessage.builder().type(MessageType.LEAVE).sender(username).build();
             messageTemplate.convertAndSend("/topic/public", chatMessage);
